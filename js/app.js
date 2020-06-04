6febbd0a8c3616f1354852468e19dbb5521debdd
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-04 10:15:41 -0500

    added JS functions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 console.log('This file connected!');
 
-var yourName = prompt("What's your name?");
+//variable decleration
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var yourName;
+var dogName;
+var message;
 
+
+//timed greeting
+if (hourNow > 18) {
+    greeting = 'Good evening,';
+}   else if (hourNow > 12){
+    greeting = 'Good afternoon, ';
+}   else if (hourNow > 0) {
+    greeting = 'Good morning, ';
+}   else {
+    greeting = 'Welcome, ';
+}
+
+//gather inputs
+ yourName = prompt("What's your name?");
+ dogName = prompt("What's your dog's name?");
+ message = ' We hope you can find something for '
+
+ //store inputs
 console.log(yourName);
+console.log(dogName);
 
-document.write(yourName);
+//write full message
+document.write('<h3>' + greeting + yourName + '!' + message + dogName + '!' + '</h3>');
