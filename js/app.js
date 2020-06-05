2563c0a0ce66c87ab310e8f5f40e2ab6aa39a060
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-05 00:43:35 -0500

    fixed CSS and JS

--- js/app.js
+++ js/app.js
@@ -1,53 +1,50 @@
 "use strict";
+function time(){
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
 
-console.log("connected!");
+    //timed greeting (taken from lab 6)
+    if (hourNow > 18) {
+        greeting = 'Good evening,';
+    }   else if (hourNow > 12){
+        greeting = 'Good afternoon, ';
+    }   else if (hourNow > 0) {
+        greeting = 'Good morning, ';
+    }   else {
+        greeting = 'Welcome, ';
+    }
+    return greeting;
+}
 
 function fullMessage(){
 
-//variable decleration
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-var yourName;
-var dogName;
-var message;
-var shoppingAmmount;
-var shoppingDescription;
+    //variable decleration
+    var yourName;
+    var dogName;
+    var message;
+    var shoppingAmmount;
+    var shoppingDescription;
+    var greeting = time();
 
-//timed greeting (taken from lab 6)
-if (hourNow > 18) {
-    greeting = 'Good evening,';
-}   else if (hourNow > 12){
-    greeting = 'Good afternoon, ';
-}   else if (hourNow > 0) {
-    greeting = 'Good morning, ';
-}   else {
-    greeting = 'Welcome, ';
+    //gather inputs
+    yourName = prompt("What's your name?");
+    dogName = prompt("What's your dog's name?");
+    shoppingAmmount = prompt("How many items are you looking for?");
+    message = ' We hope you can find ';
+
+    //change shopingDescription in full message based on the number of items looking for
+    if (shoppingAmmount >= 12){
+        shoppingDescription = 'more than a dozen items';
+    }     else if (shoppingAmmount >= 7 ){
+            shoppingDescription = 'a bunch of items';
+    }     else if (shoppingAmmount == 6){
+            shoppingDescription = 'half a dozen items';
+    }     else if (shoppingAmmount >= 1){
+            shoppingDescription = 'at least one item';
+    }      else {
+            shoppingDescription = 'something fun';
+    }
+    return greeting + ' ' + yourName + '!' + message + shoppingDescription + ' for ' + dogName + '!';
 }
-
-//gather inputs
- yourName = prompt("What's your name?");
- dogName = prompt("What's your dog's name?");
- shoppingAmmount = prompt("How many items are you looking for?")
- message = ' We hope you can find '
-
- //change shopingDescription in full message based on the number of items looking for
- if (shoppingAmmount >= 11){
-     shoppingDescription = 'more than a dozen items';
-}     else if (shoppingAmmount < 11 && shoppingAmmount >= 7 ){
-        shoppingDescription = 'a bunch of items';
-}     else if (shoppingAmmount > 0 && shoppingAmmount <= 6){
-        shoppingDescription = 'half a dozen items';
-}     else if (shoppingAmmount <= 0){
-        shoppingDescription = 'at least one item';
-}      else {
-        shoppingDescription = 'something fun';
-}
-
- //store inputs
-console.log(yourName);
-console.log(dogName);
-console.log(shoppingDescription);
-
-return '<h3>' + greeting + yourName + '!' + message + shoppingDescription + ' for ' + dogName + '!' + '</h3>';
-}
+document.getElementById('messageDisplay')
