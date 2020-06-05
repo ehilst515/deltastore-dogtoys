741fade5db2c99f1240342256d5f06482e6866e4
Enrique Hilst <enrique.hilst@gmail.com>
2020-06-05 17:32:36 -0500

    JS fixes. Added paw print loop.

--- js/app.js
+++ js/app.js
@@ -17,34 +17,48 @@ function time(){
     return greeting;
 }
 
+//ask shoppingAmmount until a number is entered
+var itemNumber = function(){
+    var shoppingAmmount = prompt("How many items are you looking for?");
+    while( shoppingAmmount ==='' || isNaN(shoppingAmmount)){
+        shoppingAmmount = prompt('You must enter the ammount of times you are looking for to shop here');
+    }
+        return Number(shoppingAmmount);
+}
+
 function fullMessage(){
 
     //variable decleration
-    var yourName;
-    var dogName;
-    var message;
-    var shoppingAmmount;
     var shoppingDescription;
     var greeting = time();
 
     //gather inputs
-    yourName = prompt("What's your name?");
-    dogName = prompt("What's your dog's name?");
-    shoppingAmmount = prompt("How many items are you looking for?");
-    message = ' We hope you can find ';
+    var yourName = prompt("What's your name?");
+    var dogName = prompt("What's your dog's name?");
+    var message = ' We hope you can find ';
+
 
     //change shopingDescription in full message based on the number of items looking for
-    if (shoppingAmmount >= 12){
+    var countOfItems = itemNumber();
+    var picture = '<img src="https://i.pinimg.com/564x/47/41/19/474119c32836b1ace813e9119d778164.jpg" alt="Paw Print" style="width:30px;height:30px;" </img>';
+    var result = '';
+    for(var i=0;i<countOfItems;i++){
+        result += picture + '<br>';
+    } 
+
+    document.getElementById('pictureArray').innerHTML = result;
+
+    if (countOfItems >= 12){
         shoppingDescription = 'more than a dozen items';
-    }     else if (shoppingAmmount >= 7 ){
+    }     else if (countOfItems >= 7 ){
             shoppingDescription = 'a bunch of items';
-    }     else if (shoppingAmmount == 6){
+    }     else if (countOfItems == 6){
             shoppingDescription = 'half a dozen items';
-    }     else if (shoppingAmmount >= 1){
+    }     else if (countOfItems >= 1){
             shoppingDescription = 'at least one item';
     }      else {
             shoppingDescription = 'something fun';
     }
-    return greeting + ' ' + yourName + '!' + message + shoppingDescription + ' for ' + dogName + '!';
+    return greeting + yourName + '!' + message + shoppingDescription + ' for ' + dogName + '!';
 }
-document.getElementById('messageDisplay')
+document.getElementById('messageDisplay').innerHTML = fullMessage();
